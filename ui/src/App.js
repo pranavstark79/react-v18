b3c09cdfd4b91dc09c1f8ae029412d6878f7d5b7
Pranav Kumar <pranavstark79@gmail.com>
2023-06-13 14:02:31 +0530

    base branch

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,27 +1,6 @@
-import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const increment = () => {
-    // let tempCount = count;
-    setCount(count +1)
-  }
-
-  const decrement = () => {
-    // let tmepCount = count;
-    setCount(count - 1)
-  }
-
-  return (
-    <div className="App">
-      <> 
-      <div>Counter: {count} </div>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      </>
-    </div>
-  );
+  return 'app';
 }
 
 export default App;
